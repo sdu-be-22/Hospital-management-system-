5f4ba2cd0f5bc45e69a4dde48cf942a9f0f8f297
Akgul18 <200103076@stu.sdu.edu.kz>
2022-04-09 12:27:27 +0600

    First commit

---
+++ hospital_management_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HospitalManagementSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hospital_management_system'
