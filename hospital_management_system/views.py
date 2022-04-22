122e600e21f8652604fb712dd7adc46f71919ba0
KGT200103415 <91022204+KGT200103415@users.noreply.github.com>
2022-04-22 09:55:57 +0600

    Commited by Gulnazik

--- hospital_management_system/views.py
+++ hospital_management_system/views.py
@@ -548,4 +548,27 @@ def getMessages(request, room):
 
 def chat_bot(request):
     return render(request, 'calc/chat_bot.html')
+
+#---------------------------------------------------------------------------------
+#------------------------ Medicines ------------------------------
+#---------------------------------------------------------------------------------
+
+
+def store(request):
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = OrderItem.objects.all()
+          # cartItems = order.get_cart_items()
+     else:
+          items = []
+          order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping': False }
+          cartItems = order['get_cart_items']
+
+     products = Product.objects.all()
+     context = {
+          'products': products,
+          # 'cartItems': cartItems
+     }
+     return render(request, 'store/store.html', context)
    
