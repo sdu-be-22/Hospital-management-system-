d76e1c1d725f32bef8b046bcba12cee5c6e04be1
Akgul18 <91001862+Akgul18@users.noreply.github.com>
2022-05-03 14:38:46 +0600

    Delete hospital_management_system directory

--- hospital_management_system/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Patient)
-admin.site.register(Doctors)
-admin.site.register(Appointment)
-admin.site.register(Medicine)
-admin.site.register(Category)
--- hospital_management_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HospitalManagementSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital_management_system'
